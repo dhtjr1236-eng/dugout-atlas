0a90c4fd17d585757c3d1d382ca385990d816e1c
K_S_SEN <dhtjr1236@gnu.ac.kr>
2026-09-20 18:54:43 +0900

    feat: enable Dugout Atlas 1.21 convenience features

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from config.logging_config import configure_logging
 from config.settings import QSS_PATH, SETTINGS, ensure_runtime_dirs
 from controllers.app_controller import AppController
 from database.sqlite_manager import SQLiteManager
+from services.convenience_features import install_convenience_features
 from services.live_scoring import install_live_scoring_support
 from services.player_enhancements import install_player_enhancements
 from services.running_metrics import install_running_support
@@ -20,6 +21,7 @@ def main() -> int:
     install_running_support()
     install_player_enhancements()
     install_live_scoring_support()
+    install_convenience_features()
     app = QApplication(sys.argv)
     app.setApplicationName(SETTINGS.app_name)
     app.setApplicationVersion(SETTINGS.app_version)
